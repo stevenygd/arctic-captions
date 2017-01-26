662c9473ac85381d94a1d4e43a4e2a3855f1e208
stevenygd <stevenygd@gmail.com>
2017-01-26 22:22:59 +0000

    [add] Batches so that we could separate the huge amount of data.

--- generate_caps_single_coco.py
+++ generate_caps_single_coco.py
@@ -38,9 +38,9 @@ def gen_model(idx, context, model, options, k, normalize, word_idict, sampling):
 
     return (idx, seq)
 
-
-def main(model, saveto, k=5, normalize=False, zero_pad=False, datasets='dev,test', sampling=False, pkl_name=None):
-# load model model_options
+def main(model, saveto, k=5, normalize=False, zero_pad=False, datasets='dev,test',
+         sampling=False, pkl_name=None, batch_id=1):
+    # load model model_options
     if pkl_name is None:
         pkl_name = model
     with open('%s.pkl'% pkl_name, 'rb') as f:
@@ -48,18 +48,17 @@ def main(model, saveto, k=5, normalize=False, zero_pad=False, datasets='dev,test
 
     # fetch data, skip ones we aren't using to save time
     load_data, prepare_data = get_dataset(options['dataset'])
-    train, valid, test, worddict = load_data(path='./data/coco/', load_train=True if 'train' in datasets else False,
-                                             load_dev=True if 'dev' in datasets else False,
-                                             load_test=True if 'test' in datasets else False)
-    
-    # import pdb; pdb.set_trace()
+    train, valid, test, worddict = load_data(path='./data/coco/',
+            load_train=True if 'train' in datasets else False,
+            load_dev=True if 'dev' in datasets else False,
+            load_test=True if 'test' in datasets else False)
+
     # <eos> means end of sequence (aka periods), UNK means unknown
     word_idict = dict()
     for kk, vv in worddict.iteritems():
         word_idict[vv] = kk
     word_idict[0] = '<eos>'
     word_idict[1] = 'UNK'
-
 
     # index -> words
     def _seqs2words(caps):
@@ -75,6 +74,11 @@ def main(model, saveto, k=5, normalize=False, zero_pad=False, datasets='dev,test
 
     # process all dev examples
     def _process_examples(contexts):
+        ttl = contexts.shape[0]
+        if batch_id > 0 and batch_id * 500 < ttl:
+            contexts = contexts[(batch_id-1)*500:batch_id*500]
+        else:
+            contexts = contexts[(ttl/500)*500:]
         caps = [None] * contexts.shape[0]
         for idx, ctx in enumerate(contexts):
             cc = ctx.todense().reshape([14*14,512])
@@ -98,26 +102,27 @@ def main(model, saveto, k=5, normalize=False, zero_pad=False, datasets='dev,test
             print 'Training Set...',
             caps = _seqs2words(_process_examples(train[1]))
             # import pdb; pdb.set_trace()
-            with open(saveto+'.train.txt', 'w') as f:
+            with open(saveto+'batch%d.train.txt'%batch_id, 'w') as f:
                 print >>f, '\n'.join(caps)
             print 'Done'
         if dd == 'dev':
             print 'Development Set...',
             caps = _seqs2words(_process_examples(valid[1]))
             # import pdb; pdb.set_trace()
-            with open(saveto+'.dev.txt', 'w') as f:
+            with open(saveto+'batch%d.dev.txt'%batch_id, 'w') as f:
                 print >>f, '\n'.join(caps)
             print 'Done'
         if dd == 'test':
             print 'Test Set...',
             caps = _seqs2words(_process_examples(test[1]))
-            with open(saveto+'.test.txt', 'w') as f:
+            with open(saveto+'batch%d.test.txt'%batch_id, 'w') as f:
                 print >>f, '\n'.join(caps)
             print 'Done'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-k', type=int, default=1)
+    parser.add_argument('-b', type=int, default=1)
     parser.add_argument('-sampling', action="store_true", default=False) # this only matters for hard attention
     parser.add_argument('-n', action="store_true", default=False)
     parser.add_argument('-z', action="store_true", default=False)
@@ -127,4 +132,4 @@ if __name__ == "__main__":
     parser.add_argument('saveto', type=str)
 
     args = parser.parse_args()
-    main(args.model, args.saveto, k=args.k, zero_pad=args.z, pkl_name=args.pkl_name, normalize=args.n, datasets=args.d, sampling=args.sampling)
+    main(args.model, args.saveto, k=args.k, zero_pad=args.z, pkl_name=args.pkl_name, normalize=args.n, datasets=args.d, sampling=args.sampling, batch_id=args.b)
